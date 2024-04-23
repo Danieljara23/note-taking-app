8b964615fda387a58d6bbbf577aa791ec6d2568e
tigarto <helpu1409@gmail.com>
2024-04-23 07:39:40 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const notesList = [
+  {
+    date: "23/11/2023",
+    title: "My super note",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure corporis dignissimos sequi assumenda velit commodi, numquam odit aliquam blanditiis"
+  },
+  {
+    date: "04/04/2024",
+    title: "Note number 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure corporis dignissimos sequi assumenda velit commodi, numquam odit aliquam blanditiis"
+  },
+  {
+    date: "24/05/2023",
+    title: "My university",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure corporis dignissimos sequi assumenda velit commodi, numquam odit aliquam blanditiis"
+  },
+]
+
+const notesElemente = document.querySelector(".notes");
+
